b1bafb5a5c700b790fc61373c81f93e045c64944
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-12 13:29:21 -0500

    really cleaned up this code, and made a lot more sense out of it.

--- hangman.py
+++ hangman.py
@@ -1,27 +1,23 @@
 import random
 
 words = ['easy', 'aa' 'beautiful', 'javascript', 'python', 'joy', 'great', 'awesome', 'read', 'reactapp', 'pomegranite', 'element', 'create']
-# answer = random.choice(words)
-answer = 'apple'
+answer = random.choice(words)
 health = 7
-print('Hangman. You know how to play, so let\'s get started!')
-
-dashes = ('_' * len(answer)) ##display = ['_'] * len(answer) ## gives you a list of underscores
-##print(' '.join(display)) ## joins list items (underscores) into a string
-print(dashes) ## a check
-
+print('Hangman. You know how to play!')
+dashes = ('_' * len(answer))
+dashes = list(dashes)
 answerletters = list(answer)
-print(answerletters)## a check
 
-##while health > 0:
-guess = input('Okay, we have ' + str(len(answer)) + ' characters, start guessing!')
-if guess in answer:
-    for char in answer: ##for index, char in enumerate(answer): ## if char == guess: dashes[index] = guess ##
-        if char == guess:
-            print(char) ##iterate to find correct letter, find() to get index of letter in answer,
-            ##then replace it at same index of dashes string using
-        else:
-            print('_')
-else:
-    health -= 1
-    if healt == 0
+while health > 0 and '_' in dashes:
+    guess = input('Okay, we have ' + str(len(answer)) + ' characters, start guessing!')
+    if guess in answer:
+        for index, char in enumerate(answer):
+            if char == guess:
+                dashes[index] = guess
+                print(dashes)
+                if '_' not in dashes:
+                    print('Nice! You won!')
+    else:
+        health -= 1
+        if health == 0:
+            print('sorry! but you lost.')
